26a7eeebf3c1e9b5a6fadf61d15651c54989f88f
Louis Maxwell <louismaxwell@knights.ucf.edu>
2020-09-02 18:23:30 -0400

    moved and updated files

--- server.js
+++ server.js
@@ -1,13 +1,60 @@
+require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const POKEDEX = require('./pokedex.json');
+
+// console.log(process.env.API_TOKEN);
 
 const app = express();
 app.use(morgan('common'));
+app.use(helmet());
+app.use(cors());
+
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
+
+// THESE GET HANDLERES ARE CALLED MIDDLEWARE, AND WE ARE ABLE TO ACCESS MULTIPLE
 
 
-app.get('/', (req,res) => {
-    res.send('Hellow, lou!');
+// WE WANT TO VALIDATE BEFORE REQUREST IS MADE TO GET
+
+app.use(function validateBearerToken(req,res,next){
+
+    const apiToken = process.env.API_TOKEN
+    const authoToken = req.get('Authorization')
+    console.log('validate bearer token middleware');
+
+
+    if(!authoToken || authoToken.split(' ')[1] !== apiToken){
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    
+    // MOVE TO THE NEXT MIDDLEWARE
+    next()
 });
+
+function handleGetTypes(req,res) {
+    res.json(validTypes);
+}
+
+function handleGetPokemon(req,res){
+    const {name, type} = req.query;
+    let response = POKEDEX.pokemon;
+
+    if(name) {
+        response = response.filter(pokemon => pokemon.name.toLowerCase().includes(name.toLowerCase()))
+    }
+    if(type){
+        response = response.filter(pokemon => pokemon.type.includes(type))
+    }
+    
+    res.json(response)
+}
+
+
+app.get('/types', handleGetTypes)
+app.get('/pokemon',handleGetPokemon)
 
 const PORT = 8000;
 
